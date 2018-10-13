115b9893e4b73c72efd59edda755c313a965b119
Jonathan Sterling <jon@jonmsterling.com>
2018-10-13 19:58:22 -0400

    more immortal L

--- theories/L.v
+++ theories/L.v
@@ -2,29 +2,34 @@ Require Import Unicode.Utf8 ssreflect.
 From Equations Require Import Equations.
 From cbpv Require Import Var Tactic.
 
+Inductive pol :=
+| POS
+| NEG.
+
+Inductive phase :=
+| FOC
+| INV.
+
 Inductive sort :=
 | CMD
-| CTX
-| STK
-| VAL
-| TM.
+| SORT : pol → phase → sort.
 
-Notation "#V" := VAL.
+
 Notation "#c" := CMD.
-Notation "#E" := STK.
-Notation "#e" := CTX.
-Notation "#v" := TM.
+Notation "#V" := (SORT POS FOC).
+Notation "#E" := (SORT NEG FOC).
+Notation "#e" := (SORT POS INV).
+Notation "#v" := (SORT NEG INV).
 
 Inductive tm (n : nat) : sort → Type :=
 | var : Var n → tm n #V
 | pair : tm n #V → tm n #V → tm n #V
-| hold_v : tm n #v → tm n #V (* curien: ↓ *)
-| ret : tm n #V → tm n #v (* curien: ↑ *)
+| hold : ∀ {π}, tm n (SORT π INV) → tm n (SORT π FOC) (* curien: ↓ *)
+| ret : ∀ {π}, tm n (SORT π FOC) → tm n (SORT π INV) (* curien: ↑ *)
 | lam : tm (S n) #c → tm n #v
 | emp : tm n #E (* ⋄ *)
 | push : tm n #V → tm n #E → tm n #E
-| hold_e : tm n #e → tm n #E
-| ask_pair : tm (S (S n)) #c → tm n #e (* μ~(x, y) *)
-| ask : tm (S n) #c → tm n #e
-| adj : tm n #c → tm n #v (* the adjoint, μ⋄.c *)
+| ask_pair : tm (S (S n)) #c → tm n #e (* μ~(x, y).c *)
+| ask : tm (S n) #c → tm n #e (* μ~x.c *)
+| adj : tm n #c → tm n #v (* c⋄ *)
 | cut : tm n #v → tm n #e → tm n #c.
